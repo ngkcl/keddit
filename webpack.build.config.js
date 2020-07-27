db0bd386e530aa96d86628ae8ae55c262a4a138a
Nick <k1889573@kcl.ac.uk>
2020-07-27 20:00:23 +0300

    fix webpack typo

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -18,7 +18,7 @@ module.exports = {
         include: defaultInclude
       },
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         use: [{ loader: 'babel-loader' }],
         include: defaultInclude
       },
